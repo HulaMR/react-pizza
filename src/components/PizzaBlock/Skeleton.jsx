567b9b6dd5bb0b785658e76577f4d05b901f7493
HulaMR <qwertymishagula13@gmail.com>
2022-10-17 22:49:31 +0300

    Context

--- src/components/PizzaBlock/Skeleton.jsx
+++ src/components/PizzaBlock/Skeleton.jsx
@@ -2,20 +2,22 @@ import React from 'react';
 import ContentLoader from 'react-content-loader';
 
 const Skeleton = () => (
-  <ContentLoader
-    className="pizza-block"
-    speed={2}
-    width={280}
-    height={457}
-    viewBox="0 0 280 457"
-    backgroundColor="#a9c9c2"
-    foregroundColor="#59aec3">
-    <circle cx="141" cy="130" r="120" />
-    <rect x="15" y="299" rx="10" ry="10" width="250" height="85" />
-    <rect x="20" y="399" rx="10" ry="10" width="75" height="30" />
-    <rect x="120" y="392" rx="30" ry="30" width="138" height="44" />
-    <rect x="15" y="265" rx="10" ry="10" width="250" height="25" />
-  </ContentLoader>
+  <div className="pizza-block-wrapper">
+    <ContentLoader
+      className="pizza-block"
+      speed={2}
+      width={280}
+      height={459}
+      viewBox="0 0 280 462"
+      backgroundColor="#a9c9c2"
+      foregroundColor="#59aec3">
+      <circle cx="141" cy="130" r="130" />
+      <rect x="15" y="275" rx="10" ry="10" width="250" height="25" />
+      <rect x="10" y="320" rx="10" ry="10" width="260" height="85" />
+      <rect x="18" y="425" rx="10" ry="10" width="80" height="35" />
+      <rect x="132" y="415" rx="25" ry="25" width="140" height="48" />
+    </ContentLoader>
+  </div>
 );
 
 export default Skeleton;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,40 +1,51 @@
 import React from 'react';
+
 import Categories from '../components/Categories';
-import Skeleton from '../components/PizzaBlock/Skeleton';
 import Sort from '../components/Sort';
+import Skeleton from '../components/PizzaBlock/Skeleton';
 import PizzaBlock from '../components/PizzaBlock';
+import Pagination from '../components/Pagination';
 import NotFound from './NotFound';
+
+import { SearchContext } from '../App';
 
 const url = 'https://633b340a471b8c39557e7c35.mockapi.io';
 
-function Home({ searchValue }) {
+function Home() {
+  const { searchValue } = React.useContext(SearchContext);
+
   const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
 
   const [categoryId, setCategoryId] = React.useState(0);
   const [sortType, setSortType] = React.useState({ name: 'популярністю↑', sortProperty: 'rating' });
 
+  const [currentPage, setCurrentPage] = React.useState(1);
+
   React.useEffect(() => {
     setIsLoading(true);
 
     const category = categoryId > 0 ? `&category=${categoryId}` : ``;
-    // const search = searchValue ? `&title=${searchValue}` : ``;        ${search}
+    const search = searchValue ? `&title=${searchValue}` : ``;
     const sortBy = sortType.sortProperty.replace('-', '');
     const order = sortType.sortProperty.includes('-') ? 'desc' : 'asc';
 
-    fetch(url + `/items?${category}&sortBy=${sortBy}&order=${order}`)
+    fetch(
+      url +
+        `/items?page=${currentPage}&limit=4${search}${category}&sortBy=${sortBy}&order=${order}`,
+    )
       .then((res) => res.json())
       .then((json) => {
         setItems(json);
         setIsLoading(false);
       });
-  }, [categoryId, sortType, searchValue]);
+  }, [categoryId, sortType, searchValue, currentPage]);
 
   const pizzas = items
-    .filter((obj) => obj.title.toLowerCase().includes(searchValue.toLowerCase()))
+    //.filter((obj) => obj.title.toLowerCase().includes(searchValue.toLowerCase()))
     .map((obj) => <PizzaBlock key={obj.id} {...obj} />);
 
-  const skeletons = [...new Array(8)].map((_, index) => <Skeleton key={index} />);
+  const skeletons = [...new Array(4)].map((_, index) => <Skeleton key={index} />);
 
   const pizzasResult = pizzas.length > 0 ? pizzas : <NotFound />;
 
@@ -46,6 +57,7 @@ function Home({ searchValue }) {
       </div>
       <h2 className="content__title">Всі піци</h2>
       <div className="content__items">{isLoading ? skeletons : pizzasResult}</div>
+      <Pagination onChangePage={(number) => setCurrentPage(number)} />
     </div>
   );
 }
